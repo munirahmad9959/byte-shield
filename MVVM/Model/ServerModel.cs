4c31294fd5eb7381b54cc1fa26a7d77120831955
ZavAhmed <2022cs614@student.uet.edu.pk>
2025-01-06 23:04:09 +0500

    Frontend Done now remaining add servers and connecting functionality...

---
+++ MVVM/Model/ServerModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteShield.MVVM.Model
+{
+    public class ServerModel
+    {
+        public int ID { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Server { get; set; }
+        public string? Country { get; set; }
+    }
+
+}
---
+++ MVVM/ViewModel/ProtectionViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteShield.MVVM.Model;
+
+namespace ByteShield.MVVM.ViewModel
+{
+    internal class ProtectionViewModel
+    {
+        public ObservableCollection<ServerModel> Servers { get; set; }
+
+        public ProtectionViewModel()
+        {
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                });
+            }
+        }
+
+    }
+}
